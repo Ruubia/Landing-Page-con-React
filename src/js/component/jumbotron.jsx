553c4234c5f06ed2c292acb32922a020f403ed53
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-14 15:01:20 +0000

    cambie class= por className=

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -6,7 +6,7 @@ const Jumbotron = () => {
         <div className="jumbotron">
         <h1 className="text-left">A Warm Welcome!</h1>
         <p className="lead text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non arcu eu tortor pretium venenatis at nec mauris. Suspendisse ut bibendum odio. In sed suscipit neque. In congue erat non arcu imperdiet condimentum. Nullam vulputate nisl nec dolor laoreet, quis semper nisi tristique.</p>
-        <button class="btn btn-primary btn-lg" type="button">Call to action!</button>
+        <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
         <hr className="my-4"/>
       </div>
        
